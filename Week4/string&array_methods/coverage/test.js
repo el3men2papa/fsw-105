93cdb593a8b2787c83769239a9624241565ad159
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-25 01:16:11 -0400

    first commit

---
+++ Week4/string&array_methods/coverage/test.js
@@ -0,0 +1,60 @@
+var str = "HELLO WORLD";
+var res = str.toLowerCase();
+
+console.log(res)
+
+
+var strs = "hello world";
+var rest = str.toUpperCase();
+
+console.log(rest)
+
+
+function name(strs){
+console.log(rest)
+}
+
+name()
+
+
+var strse = "hello world";
+var reste = str.capitalized();
+var lower = str.LowerCase();
+
+console.log(reste + "" + lower)
+
+
+function names(strse){
+console.log(reste)
+}
+
+name()
+
+
+
+
+
+
+
+//Follow up
+
+var capitalizedAndLowerCase = "Hello world";
+
+
+function final(a){
+  if(a.length%2===0){
+var short = a.slice(0, a.length/2);
+
+  }
+else {
+var firstHalf = a.slice(0, a.length/2);
+var sencondHalf = a.slice(a.length/2);
+var firstUpper = firstHalf.toUpperCase();
+var secondLower = sencondHalf.toLowerCase()
+console.log(firstUpper.concat(secondLower))
+}
+
+}
+
+
+final(capitalizedAndLowerCase)
