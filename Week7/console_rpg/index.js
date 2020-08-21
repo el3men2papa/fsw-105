c98d7861302962f055fb99858220987f7b1e29aa
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-08-21 03:28:01 -0400

    first commit

--- Week7/console_rpg/index.js
+++ Week7/console_rpg/index.js
@@ -1,16 +1,23 @@
 //This is the original code for the project the others js files are for manipulations
 
+  
+  
+
 let readlineSync = require('readline-sync');
-let items =  ["15 health", "75 gold", "1 ring", "1 sword"];;
+let nickName = readlineSync.question("Create a user name: ");
+  let age = readlineSync.question("Please enter your age: ");
+let items =  ["15 health", "75 gold", "1 ring", "1 sword"];
 let walk = "Keep Playing";
 let optionOne = "1. Walk"
 let optionTwo = "2. Run"
 let optionThree = "3. Attack"
 let userQuestion = "What do you want to do?"
 let kuppa = "Kuppa"
+let pig = "pig"
+let jackle = "jackle"
 let keepPlaying = "Keep Walkign"
 let run = ["You have Scape keep walking", "There is no choice than to fight"]
-let enemiesList = [keepPlaying, kuppa, keepPlaying];
+let enemiesList = [keepPlaying, kuppa, keepPlaying, pig, keepPlaying, jackle];
 let won = false;
 let lost = false;
 let max = 35;
@@ -20,18 +27,23 @@ let attackPlayer = Math.floor(Math.random() * (max - min)) + min;
 let randomItems = Math.floor(Math.random() * items.length);
 let player = 100;
 let boss = 100;
+let oneTime = true
 
+
+console.log("Ready for your worst night mare", nickName, "?")
  while (!won && !lost ){
+
+
   let generalQuestions1 = readlineSync.question(userQuestion.concat(" ",optionOne))
-  
+ 
 
   keepPlaying = enemiesList [Math.floor(Math.random() * enemiesList.length)] 
-if(generalQuestions1 == "1" && keepPlaying != kuppa){
+if(generalQuestions1 == "1" && keepPlaying != kuppa && keepPlaying != pig && keepPlaying != jackle){
   console.log(keepPlaying)
 }
 
- else if(keepPlaying === kuppa){
-  console.log("You have found an enemy")
+ else if(keepPlaying === kuppa || keepPlaying === jackle || keepPlaying === pig){
+  console.log("You have found", keepPlaying )
   let generalQuestions2 = readlineSync.question(userQuestion.concat(" ", optionTwo, " ", optionThree))
   let test =  Math.floor(Math.random() * run.length)
   if (generalQuestions2 == "2"){
@@ -39,7 +51,7 @@ if(generalQuestions1 == "1" && keepPlaying != kuppa){
     console.log( run[test])
     } 
     if(generalQuestions2 == "3" ||  run[test] === "There is no choice than to fight"){
-      console.log("Get ready to attack")
+      console.log(keepPlaying, "VS", nickName,"Get ready to attack")
       while (boss >= 0 && player >= 0){
         let generalQuestions3 = readlineSync.question(userQuestion.concat(" ", optionThree))
         if(generalQuestions3 == "3"){
@@ -50,10 +62,10 @@ if(generalQuestions1 == "1" && keepPlaying != kuppa){
         boss = Math.floor(boss - attackPlayer) 
         }
 
-       console.log("Player attack point -",attackPlayer, "to Boss HP")
-       console.log("Boss HP",boss)
-       console.log("Boss attack point -",attackBoss, "to player HP" )
-       console.log("Player HP",player)
+       console.log("Player attack point -",attackPlayer, "to", keepPlaying, "HP")
+       console.log(keepPlaying,"HP",boss)
+       console.log(keepPlaying,"attack point -",attackBoss, "to", nickName ,"HP" )
+       console.log(nickName ,"HP",player)
       }
     }
 } 
@@ -61,18 +73,65 @@ if (player <= 0 && boss >= 0){
   won = false
   lost = true
   console.log("You lost")
+  console.log("  ..####....####...##...##..######. ")                              
+  console.log(".##......##..##..###.###..##.....     ")                          
+  console.log(".##.###..######..##.#.##..####...       ")                        
+  console.log(".##..##..##..##..##...##..##.....         ")                      
+  console.log("..####...##..##..##...##..######.           ")                    
+  console.log(".................................             ")                  
+  console.log("..................................####...##..##..######..#####..")
+  console.log(".................................##..##..##..##..##......##..##.")
+  console.log(".................................##..##..##..##..####....#####..")
+  console.log(".................................##..##...####...##......##..##.")
+  console.log("..................................####.....##....######..##..##.")
+  console.log("...............................................................")
 } 
 else if (boss <= 0 && player >=0 ){
- let randomItems = Math.floor(Math.random() * items.length);
- console.log ("You have collected ",items[randomItems])
+ 
   
-  console.log("You Won!!!")
+  console.log(nickName, "You Won!!!")
+  console.log(nickName, "HP", player, )
+  let randomItems = Math.floor(Math.random() * items.length);
+ console.log ("You have collected ",items[randomItems])
+
+                                                           
+ console.log(" @@@ @@@   @@@@@@   @@@  @@@  ")                              
+ console.log("@@@ @@@  @@@@@@@@  @@@  @@@     ")                           
+ console.log("@@! !@@  @@!  @@@  @@!  @@@       ")                         
+ console.log("!@! @!!  !@!  @!@  !@!  @!@         ")                       
+ console.log(" !@!@!   @!@  !@!  @!@  !@!           ")                     
+ console.log("  @!!!   !@!  !!!  !@!  !!!             ")                   
+ console.log("  !!:    !!:  !!!  !!:  !!!               ")                 
+ console.log("  :!:    :!:  !:!  :!:  !:!                 ")               
+ console.log("   ::    ::::: ::  ::::: ::                   ")             
+ console.log("   :      : :  :    : :  :                      ")           
+                                                            
+                                                            
+console.log("         @@@  @@@  @@@   @@@@@@   @@@  @@@  @@@  @@@  @@@ ") 
+console.log("          @@@  @@@  @@@  @@@@@@@@  @@@@ @@@  @@@  @@@  @@@  ")
+console.log("          @@!  @@!  @@!  @@!  @@@  @@!@!@@@  @@!  @@!  @@!  ")
+console.log("          !@!  !@!  !@!  !@!  @!@  !@!!@!@!  !@   !@   !@   ")
+console.log("          @!!  !!@  @!@  @!@  !@!  @!@ !!@!  @!@  @!@  @!@  ")
+console.log("          !@!  !!!  !@!  !@!  !!!  !@!  !!!  !!!  !!!  !!!  ")
+console.log("          !!:  !!:  !!:  !!:  !!!  !!:  !!!                 ")
+console.log("          :!:  :!:  :!:  :!:  !:!  :!:  !:!  :!:  :!:  :!:  ")
+console.log("           :::: :: :::   ::::: ::   ::   ::   ::   ::   ::  ")
+console.log("            :: :  : :     : :  :   ::    :   :::  :::  :::  ")
+                                                          
    won = true
    lost= false
 
  }  
  else if(boss <=0 && player <=0 ){
    lost = true
-   console.log("Its a tie")
- }
+   console.log(nickName,"lost", "&", keepPlaying, "lost")
+
+console.log(" ▄▀▀█▀▄    ▄▀▀▀█▀▀▄  ▄▀▀▀▀▄      ▄▀▀█▄       ▄▀▀▀█▀▀▄  ▄▀▀█▀▄   ▄▀▀█▄▄▄▄ ")
+console.log(" █   █  █  █    █  ▐ █ █   ▐     ▐ ▄▀ ▀▄     █    █  ▐ █   █  █ ▐  ▄▀   ▐ ")
+console.log("  ▐   █  ▐  ▐   █        ▀▄         █▄▄▄█     ▐   █     ▐   █  ▐   █▄▄▄▄▄  ")
+console.log("      █        █      ▀▄   █       ▄▀   █        █          █      █    ▌  ")
+console.log("   ▄▀▀▀▀▀▄   ▄▀        █▀▀▀       █   ▄▀       ▄▀        ▄▀▀▀▀▀▄  ▄▀▄▄▄▄   ")
+console.log("  █       █ █          ▐          ▐   ▐       █         █       █ █    ▐   ")
+console.log("  ▐       ▐ ▐                                 ▐         ▐       ▐ ▐      ")
+    }
   }
